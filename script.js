410b6fe4b3c5ebf6142c67e1e925377f1bc86b46
Kucing1221 <131422630+Kucing1221@users.noreply.github.com>
2024-04-21 23:40:01 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function tampilkanPesan() {
+    var pesan = "Happy 2 years anniversary of our relationship my angelll. On April 22, 2022, we began this journey of love, a journey filled with beauty even though we have never met in person. Our relationship is proof that love knows no bounds, not hindered by distance or time. Over these past two years, we have gone through a lot together. Although there are occasional small arguments, we always find a way to resolve them and come back stronger. This shows how deep our understanding and love for each other are. Every day, our love deepens, strengthening the bond we have built with patience and dedication. I can't wait for May 13, 2024, counting from today, in 21 days when we will meet for the first time. I am sure it will be one of the most beautiful days of our lives. Let's continue to keep this love with all tenderness and warmth my only future wife.";
+
+    var pesan2 = "Baharın müjdecisi nergisler açarken, Seninle başladığımız bu yolculukta, Her bir yaprak gibi katman katman açıldı kalbim. Gözlerinin derinliğinde kaybolduğum, Ve sesinin melodisinde huzur bulduğum, Seninle her bahar daha anlamlı, her gün daha değerli. Nergisler gibi, uzun kışın ardından, Yeniden doğuyoruz her gün birlikte, Ve her anı sımsıkı sarılıp, değerini biliyoruz. Seninle geçen her gün, bir ömre bedel, Ve bil ki, her daim yanındayım, Gönülden gönüle bir köprü kurarak." 
+    
+    var pesan3 = "I hope this poem can represent my feelings of love for you, and may the daffodil flowers, symbolizing rebirth and eternity, also symbolize our love that will continue to grow and flourish over time. Happy anniversary to our 2 years relationship my love. I love you with all my heart my everythingg.";
+
+
+    document.getElementById("pesan").innerHTML = "<p>" + pesan + "</p><p>" + pesan2 + "</p><p>" + pesan3 + "</p>" ;
+}   
+function hidupkanSuara() {
+    
+    var audio = document.querySelector('audio');
+    audio.muted = false;
+    audio.play();
+
+
+}
